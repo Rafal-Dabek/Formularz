db678cc74f09667b4f6b39905ba999540c076c5f
Rafal-Dabek <62549937+Rafal-Dabek@users.noreply.github.com>
2021-10-17 22:27:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -148,18 +148,23 @@ function done(){
   var mentor=0;
   var coach=0;
   var doradca=0;
-  
+  var d="";
+  var ele = document.getElementsByTagName('input');
     
-    
-       /*
-        if(document.querySelector(
-          'input[name="Question"]:checked')){
-          mentor +=parseInt( document.getElementById(9 + "_"+"3" +"_" +"1" ).value);
-          coach+=parseInt( document.getElementById(9 + "_"+"2" +"_" +"1" ).value);
-          doradca+=parseInt( document.getElementById(9 + "_"+"1" +"_" +"1" ).value);
+  /*for(i = 0; i < ele.length; i++) {
+                  
+    if(ele[i].type="radio") {
+      
+        if(ele[i].checked){
+          
+          d+= parseInt(ele[i].value);
 
-          console.log(mentor);
-        }*/
+
+
+        }
+            
+    }
+}*/
        
 
 
@@ -168,9 +173,11 @@ function done(){
         var c=[];
         var b=[];
         var a=[];
-        var d;
+        var d=[];
         for(i = 0; i < ele.length; i++) {
-              
+              var m=0;
+              var k=0;
+              var r=0;
             if(ele[i].type="radio") {
               
                 if(ele[i].checked){
@@ -178,16 +185,21 @@ function done(){
                   document.getElementById("disp").innerHTML
                   += ele[i].className + " Points: "
                   + ele[i].value + "<br>";
-                  if(ele[i].className=="C"&&ele[i].checked){
-                     c[i]= parseInt(ele[i].value);
+                  //a[i]=ele[i].value;
+                  if(ele[i].value!=null)d+=ele[i].value;
+                  if(ele[i].className=="C"&&ele[i].checked&&ele[i].value!=null){
+                     c[m]= parseInt(parseInt(ele[i].value));
                      // console.log(c[i]);
+                     m++;
                      }
                   if(ele[i].className=="B"&&ele[i].checked){
-                    b[i]= parseInt(ele[i].value);
+                    b[r]= parseInt((ele[i].value));
+                    r++;
                     //console.log(b[i]);
                   }
                   if(ele[i].className=="A"&&ele[i].checked){
-                      a[i]= ele[i].value;
+                      a[k]= parseInt((ele[i].value));
+                      k++;
                       //console.log(a[i]);
                     }
 
@@ -201,20 +213,86 @@ function done(){
           }
 
 
-coach=c[0]+b[1]+b[2]+a[3]+c[4]+a[5]+a[6]+b[7]+b[8]+b[9];
-mentor=a[0]+c[1]+a[2]+c[3]+a[4]+c[5]+c[6]+c[7]+a[8]+c[9];
-doradca=b[0]+a[1]+c[2]+b[3]+b[4]+b[5]+b[6]+a[7]+c[8]+a[9];
-for(i=0;i<a.length;i++){
-  console.log(a[i]);
+coach=d[2]+d[4]+b[2]+a[3]+c[4]+a[5]+a[6]+b[7]+b[8]+b[9];
+//mentor=a[0]+c[1]+a[2]+c[3]+a[4]+c[5]+c[6]+c[7]+a[8]+c[9];
+//doradca=b[0]+a[1]+c[2]+b[3]+b[4]+b[5]+b[6]+a[7]+c[8]+a[9];
+for(i=0;i<d.length;i++){
+  var l=0;
+  var j=0;
+  var m=0;
+
+  /*
+  if(i%3==0&&d[i]!=null){
+    a[l]=d.slice(0+i,1+i );
+  l++;
+ 
+  
+  }
+  if(i%3==1){
+    b[j]=d.slice(1+i,2+i );
+  j++;
+  }
+  if(i%3==2){
+    c[m]=d.slice(2+i,3+i );
+  m++;
+  }*/
+
+  
+  
+  
+  
+
+
+
+
+  /*
+  a[0]=d.slice(0,1)
+  a[1]=d.slice(0,1)
+  a[2]=d.slice(0,1)
+  a[3]=d.slice(0,1)
+  a[4]=d.slice(0,1)
+  a[5]=d.slice(0,1)
+  a[6]=d.slice(0,1)
+  a[7]=d.slice(0,1)
+  a[8]=d.slice(0,1)
+  a[9]=d.slice(0,1)
+  c[0]=d.slice(0,1)
+  c[1]=d.slice(0,1)
+  c[2]=d.slice(0,1)
+  c[3]=d.slice(0,1)
+  c[4]=d.slice(0,1)
+  c[5]=d.slice(0,1)
+  c[6]=d.slice(0,1)
+  c[7]=d.slice(0,1)
+  c[8]=d.slice(0,1)
+  c[9]=d.slice(0,1)
+
+
+
+  b[0]=d.slice(1,2)
+  b[1]=d.slice(0,1)
+  b[2]=d.slice(0,1)
+  b[3]=d.slice(0,1)
+  b[4]=d.slice(0,1)
+  b[5]=d.slice(0,1)
+  b[6]=d.slice(0,1)
+  b[7]=d.slice(0,1)
+  b[8]=d.slice(0,1)
+  b[9]=d.slice(0,1)
+  */
+  
   //d+=parseInt(a[i]);
+  console.log(a)
   }
 //console.log(coach);
 //console.log(mentor);
 //console.log(doradca);
-//console.log(d)
+//console.log(a)
+//console.log(a[0])
+//console.log(b)
+//console.log(c)
+
 }
-
-
 
 
 
